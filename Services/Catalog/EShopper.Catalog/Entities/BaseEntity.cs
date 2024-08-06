aaf5f9b9832a8feae905c1327f76b86dae95ac65
bedialp <fvzlpktk@gmail.com>
2024-08-06 22:38:05 +0300

    Add project files.

---
+++ Services/Catalog/EShopper.Catalog/Entities/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace EShopper.Catalog.Entities
+{
+  public class BaseEntity
+  {
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+  }
+}
---
+++ Services/Catalog/EShopper.Catalog/Services/CategoryServices/ICategoryService.cs
@@ -0,0 +1,10 @@
+﻿using EShopper.Catalog.Entities;
+using EShopper.Catalog.Repositories;
+
+namespace EShopper.Catalog.Services.CategoryServices
+{
+  public interface ICategoryService : IRepository<Category>
+  {
+
+  }
+}
